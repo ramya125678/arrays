694e0b4a322ab51d2a718f3b0c146125f70d04e6
Ramya <ramyasrilaxmi@gmail.com>
2022-07-06 16:02:21 -0700

    adding arrays

---
+++ src/Arrayspractice/arrayss.java
@@ -0,0 +1,27 @@
+package Arrayspractice;
+
+public class arrayss {
+    static int a[]={10,20,30,40,50,60};
+     static  int b[]=new int[6];
+
+    public static void main(String[] args) {
+
+         b[0]=10;
+         b[1]=20;
+         b[2]=30;
+         b[3]=40;
+         b[4]=50;
+         b[5]=60;
+
+         System.out.println(a.length);
+        System.out.println(b[4]);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        System.out.println(b[3]);
+        System.out.println(b[4]);
+        System.out.println(b[5]);
+
+
+    }
+}
